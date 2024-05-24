9db6e958255c540463e2e05c2635c6f12b3fa630
Henock <hlungelehenock@gmail.com>
2024-05-24 12:44:56 +0200

    creation de la methode de recherche

--- src/stores/storePersonneN.js
+++ src/stores/storePersonneN.js
@@ -2,5 +2,25 @@ import { defineStore } from "pinia";
 import axios from "axios";
 
 export const usePersonneN = defineStore("uPersN", {
-
-})
+  state: () => ({
+    personne: [],
+  }),
+  getters: {},
+  actions: {
+    async getpers(s) {
+      let f = new FormData();
+      f.append("nom", s.nom);
+      f.append("postnom", s.postnom);
+      f.append("prenom", s.prenom);
+      f.append("date_naissance", s.date_naissance);
+      f.append("sexe", s.sexe);
+      f.append("lieu_naissance", s.lieu_naissance);
+      f.append("num_maison", s.num_maison);
+      let data = await axios.post(
+        "http://localhost/sigp/personne/get_by_names/?user=hibaigle&mdp=mdp",
+        f
+      );
+      return data;
+    },
+  },
+});
