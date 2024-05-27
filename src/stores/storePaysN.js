90a1901a8d0b2c8dd7582ceb4a5b1a0a1c3c0c02
Kabanangi <emmanuelkabanangi@gmail.com>
2024-05-27 11:01:17 +0200

    ajout methode d'enregistrement pays dans store

--- src/stores/storePaysN.js
+++ src/stores/storePaysN.js
@@ -13,4 +13,13 @@ export const usePaysN = defineStore("uPaysN", {
       return data;
     },
   },
+  async postpays(p){
+    let d = new FormData();
+    d.append("nom", p.nom);
+    let data = await axios.post(
+      "http://localhost/sigp/pays/save?user=hibaigle&mdp=mdp",
+      d
+    );
+    return data;
+  }
 });
